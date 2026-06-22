000500588049fc9341c69f5190b816b78d50fab4
kimjungwook <tea_meen1@daum.net>
2026-06-22 10:43:02 +0900

    fix: CI 빌드오류 수정 (fundinge2e testcode)

--- backend/src/test/java/com/team04/domain/funding/FundingPaymentE2ETest.java
+++ backend/src/test/java/com/team04/domain/funding/FundingPaymentE2ETest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -31,6 +32,7 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class FundingPaymentE2ETest {
 
